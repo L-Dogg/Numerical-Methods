0ed2a20091d8c5de845e1b188927cfaa66764274
orgiele <szymek94sul@gmail.com>
2015-12-12 11:38:33 +0100

    Initial commit for 5th labs
    Condition number for A=A^t diagonally dominant matrices

---
+++ MN2/lab5/condition_number.m
@@ -0,0 +1,14 @@
+function cnd = condition_number(A)
+%CONDITION_NUMBER - obliczanie wskaznika uwarunkowania macierzy A=A^t i
+%diagonalnie silnie dominujacej. Do obliczenia lambda_max nalezy zastosowac
+%zwykla metode potegowa, a do obliczenia lambda_min odwrotna metode
+%potegowa. Odpowiedni uklad rownan nalezy rozwiazac metoda iteracyjna
+%Jacobiego.
+
+x_0 = randi(30, length(A), 1); %losowy wektor poczatkowy
+x_1;
+
+
+
+end
+
