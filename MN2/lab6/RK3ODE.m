3b538e1e6cea99ad289b67ae52922c4a2bd51815
L-Dogg <szymek94sul@gmail.com>
2015-12-20 14:12:12 +0100

    Totally new method!
    Now you can solve:
    y'(x) = f(x, y(x), z(x))
    z'(x) = g(x, y(x), z(x))

---
+++ MN2/lab6/RK3ODE.m
@@ -0,0 +1,51 @@
+function [y,z] = RK3ODE(f, g, a, b, n, y_a, z_a)
+%RK3 - metoda Rungego-Kutty rzedu 3-ego(alfa=1, Beta = 1/2) 
+%      dla rownania y'=f(x,y)
+%Argumenty:
+%   f    - uchwyt do funkcji f(x,y,z)
+%   g    - uchwyt do funkcji g(x,y,z)
+%   a, b - poczatek i koniec przedzialu
+%   n    - liczba wezlow
+%   y_a  - warunek poczatkowy y(a) = y_a
+%   z_a  - warunek pocz?tkowy z(a) = z_a
+
+
+%      y'(x) = f(x,y(x),z(x))
+%      z'(x) = g(x,y(x),z(x))
+
+alfa = 1;
+beta = 1/2;
+
+h = (b-a)/n;
+x = linspace(a, b, n+1);
+y = zeros(1, length(x));
+y(1) = y_a;
+z = zeros(1, length(x));
+z(1) = z_a;
+
+coef = [1+ (2-3*(alfa+beta))/(6*alfa*beta),0,0;...
+    (3*beta-2)/(6*alfa*(beta-alfa)), alfa, 0;...
+    (2-3*alfa)/(6*beta*(beta-alfa)), (3*alfa*beta*(1-alfa)-beta^2)/(alfa*(2-3*alfa)),...
+    (beta*(beta-alfa))/(alfa*(2-3*alfa))];
+%coef = [1/6, 0, 0; 1/6, 1, 0; 2/3, 1/4, 1/4];
+
+k = zeros(1, 3);
+l = zeros(1,3);
+
+for m = 2:length(x)
+    k(1) = f(x(m-1), y(m-1), z(m-1));
+    l(1) = g(x(m-1), y(m-1), z(m-1));
+    
+    k(2) = f(x(m-1) + h, y(m-1) + k(1) * h, z(m-1) + l(1) * h);
+    l(2) = g(x(m-1) + h, y(m-1) + k(1) * h, z(m-1) + l(1) * h); 
+    
+    k(3) = f(x(m-1) + h/2, y(m-1) + h * ( 1/4 * k(1) + 1/4 * k(2)),...
+        z(m-1) + h * ( 1/4 * l(1) + 1/4 * l(2)));   
+    l(3) = g(x(m-1) + h/2, y(m-1) + h * ( 1/4 * k(1) + 1/4 * k(2)), ...
+        z(m-1) + h * ( 1/4 * l(1) + 1/4 * l(2)));
+    
+    y(m) = y(m-1) + 1/6 * h * (k(1) + 4*k(2) + k(3));
+    z(m) = z(m-1) + 1/6 * h * (l(1) + 4*l(2) + l(3));
+end
+
+end
---
+++ MN2/lab6/test_rk3ode.m
@@ -0,0 +1,53 @@
+function test_rk3ode(s, n)
+%testRK3ODE - testuje funkcje RK3ODE na rozwiazanych przykladach
+%Argumenty:
+%         s - liczba 1, 2, 3 czyli numer przykladu
+%         n - liczba wezlow
+%1)y'=z      y(0)=1
+%  z'=-2y-z  z(0)=1
+%2)y'=y+5z   y(0)=2
+%  z'=-y-3z  z(0)=1
+%3)y'=2y     y(0)=2
+%  z'=y*z^2  z(0)=-0.18556125259086278268524173840442697991712035885370
+switch s
+    case 1
+        %y(x)= e^(-x) * (2x+1)
+        %z(x)= e^(-x) * (1-2x)
+        [y,z] = RK3ODE(@(x,y,z)z, @(x,y,z)-y-2*z, 0, 10, n, 1, 1);
+        x = linspace(0, 10, n+1);
+        u = @(x) exp(-1*x) .* (2*x+1);
+        w = @(x) exp(-1*x) .* (-2*x+1);
+    case 2
+        %y(x)= e^(-x) * (9sinx+2cosx)
+        %z(x)= e^(-x) * (cosx-4sinx)
+        [y,z] = RK3ODE(@(x,y,z)y+5*z, @(x,y,z)-y-3*z, 0, 10, n, 2, 1);
+        x = linspace(0, 10, n+1);
+        u = @(x) exp(-1*x) .* (9*sin(x)+2*cos(x));
+        w = @(x) exp(-1*x) .* (cos(x)-4*sin(x)); 
+    case 3
+        %y(x)= e^(2x) * 2
+        %z(x)= 1/(2-e^(2x))
+        [y,z] = RK3ODE(@(x,y,z)2*y, @(x,y,z)y*z^2, 1, 5, n, 2*exp(2),...
+            -0.18556125259086278268524173840442697991712035885370);
+        x = linspace(1, 5, n+1);
+        u = @(x) exp(2.*x) .* 2;
+        w = @(x) 1 ./(2.-exp(2.*x)); 
+end
+
+f = figure('name', 'y(x)', 'NumberTitle','off');
+hold on;
+plot(x, y, 'g');
+plot(x, u(x), 'r');
+legend('y*(x)', 'y(x)');  
+hold off;
+movegui(f, 'north');
+
+g = figure('name', 'z(x)', 'NumberTitle','off');
+plot(x, z, 'g');
+hold on;
+plot(x, w(x), 'r');
+legend('z*(x)', 'z(x)');  
+hold off;
+movegui(g, 'south');
+end
+
