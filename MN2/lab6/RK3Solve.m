bacb049368dc9f6e937e86d9346dcb70e76eaef8
L-Dogg <szymek94sul@gmail.com>
2015-12-20 06:48:20 +0100

    system solver initial commit

---
+++ MN2/lab6/RK3Solve.m
@@ -0,0 +1,13 @@
+function RK3Solve(A, c, a, b, n, y_a)
+%RK3Solve - metoda Rungego-Kutty rzedu trzeciego dla ukladu dwoch rownan
+%Argumenty:
+%   A   - macierz ukladu
+%   c   - kolumna wyrazow wolnych
+%   a,b - poczatek i koniec przedzialu
+%   n   - liczba wezlow
+%   y_a - warunek poczatkowy y(a) = y_a
+
+pom = [1,1]';
+
+end
+
